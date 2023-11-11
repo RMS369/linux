f27a1bd0f869f7c62cc4e9946caf2a61a70b76bf
com20 <com20.gmail.com>
2023-11-11 11:28:49 +0900

    반가워 커밋

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 const PORT = 8080;
 app.listen(PORT, function() {
-    console.log('hello world');
+    console.log('hello 반가워');
     console.log('hi');
     console.log('Listening on port: ', PORT);
 });
